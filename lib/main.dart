d11932936a09b44da97f47e8884d3648ed72702f
SyedUzair42101 <uzairhassan206@gmail.com>
2022-05-28 16:52:20 +0500

    saturday1

--- lib/main.dart
+++ lib/main.dart
@@ -105,49 +105,6 @@ class MyHomePage extends StatelessWidget {
               ],
             ),
           ),
-        )
-//       Query(
-//         options: QueryOptions(
-//           document: gql(ResturantQueries.getResturantsAll),
-//         ),
-//         builder: (QueryResult result,
-//             {VoidCallback? refetch, FetchMore? fetchMore}) {
-//           if (result.hasException) {
-//             // if there is error in response then show error
-//             return Text(result.exception.toString());
-//           }
-
-//           if (result.isLoading) {
-//             // if response is loading then show loading indicator
-//             return const Text('Loading');
-//           }
-
-//           // it can be either Map or List
-//           // List<Book> books = [];
-
-//           // var _listOfBooksJson = json.decode(result.data!['list']);
-
-//           // print("result data ${_listOfBooksJson}");
-
-//           // return SizedBox(
-//           //   child: Text("Data is loaded"),
-//           // );
-
-//           List resturantsList = result.data!['list'];
-//           print(resturantsList);
-// //         // print the response in ListView using ListView.builder
-//           return ListView.builder(
-//             itemCount: resturantsList.length,
-//             itemBuilder: (context, i) {
-//               return ListTile(
-//                 title: Text(resturantsList[i]['name']),
-//                 subtitle: Text(resturantsList[i]['location']),
-//                 trailing: Text(resturantsList[i]['item']['items']),
-//               );
-//             },
-//           );
-//         },
-//       ),
-        );
+        ));
   }
 }
